3ab3cfc38ba5fc52bd29ec06b68c08da260196af
jeongwooki <jeongwooki92@gmail.com>
2022-03-11 18:17:15 +0900

    reservation

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const User = require("./user");
 const Car = require("./car");
+const Reservation = require("./reservation");
 
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
@@ -17,11 +18,14 @@ db.sequelize = sequelize;
 
 db.User = User;
 db.Car = Car;
+db.Reservation = Reservation;
 
 User.init(sequelize);
 Car.init(sequelize);
+Reservation.init(sequelize);
 
 User.associate(db);
 Car.associate(db);
+Reservation.associate(db);
 
 module.exports = db;
